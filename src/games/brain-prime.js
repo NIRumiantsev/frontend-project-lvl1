a77434012da4f3da1042a991be1e4dc87f0c6488
truehusla <nirumiantsev@gmail.com>
2020-04-06 18:41:38 +0300

    Update brain-prime.js

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -8,15 +8,19 @@ const getQuestionAndAnswer = () => {
 
     const question = generateRandomNumber(100, 0);
 
-    let divisor = 1;
+    const checkPrime = () => {
+        let divisor = 1;
 
-    for(let ind = 2; ind <= question && question % ind !== 0 ; ind++) {
+        for(let ind = 2; ind <= question && question % ind !== 0 ; ind++) {
 
-        divisor = ind;
+            divisor = ind;
 
-    }
+        }
 
-    let correctAnswer = divisor === question ? 'yes' : 'no';
+        return  divisor === question;
+    };
+
+    const correctAnswer = checkPrime() ? 'yes' : 'no';
 
     return [question, correctAnswer];
 
